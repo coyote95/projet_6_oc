c4ebf4abcdf951d2b987cfc06da584b344e38785
coyote95 <marie.guillot.oc@gmail.com>
2023-11-20 19:33:36 +0100

    index

--- script.js
+++ script.js
@@ -1,33 +1,70 @@
-function getAllMovies(url) {
-    $.ajax({
-        url: url,
-        method: 'GET',
-        success: function (data) {
-            const movies = data.results;
+const itemsPerPage = 1; // Défiler une image à la fois
+let currentPage = 0; // Page actuelle
+const maxPages = 10; // Nombre maximum de pages à charger
 
-            // Effacer le contenu précédent
-            // $('#movie-years').empty();
+function displayMovieImages(url) {
+  if (currentPage < maxPages) {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const movies = data.results;
 
-            // Afficher les années des films filtrés par année
-            movies.forEach(function (movie) {
-                const movieYear = `
-                  
-                        Année de "${movie.title}": ${movie.year}
-                    `;
-                $('#movie-years').append(movieYear);
-            });
+        // Sélectionner l'élément conteneur pour le carrousel
+        const carouselContainer = document.getElementById('carousel');
 
-            // Vérifier s'il y a une page suivante
-            if (data.next !== null) {
-                // Appel récursif pour la page suivante
-                getAllMovies(data.next);
-            }
-        },
-        error: function () {
-            console.error('Erreur lors de la requête Ajax');
+        // Ajouter les images au carrousel
+        movies.forEach(function (movie) {
+          const carouselItem = document.createElement('div');
+          carouselItem.className = 'carousel-item';
+          const img = document.createElement('img');
+          img.src = movie.image_url;
+          img.alt = movie.title;
+          carouselItem.appendChild(img);
+          carouselContainer.appendChild(carouselItem);
+        });
+
+        // Vérifier s'il y a une page suivante
+        if (data.next !== null) {
+          // Appel récursif pour la page suivante seulement si la limite n'est pas atteinte
+          currentPage++;
+          displayMovieImages(data.next);
+        } else {
+          // Mettre à jour le carrousel une fois toutes les images chargées
+          updateCarousel();
         }
-    });
+      })
+      .catch(error => {
+        console.error('Erreur lors de la requête Fetch', error);
+      });
+  }
 }
 
 // Appel initial avec l'URL de la première page
-getAllMovies('http://localhost:8000/api/v1/titles/');
+displayMovieImages('http://localhost:8000/api/v1/titles/');
+
+function nextSlide() {
+  const carouselItems = document.querySelectorAll('.carousel-item');
+  const pageCount = Math.ceil(carouselItems.length / itemsPerPage);
+
+  if (currentPage < pageCount - 1) {
+    currentPage++;
+  } else {
+    currentPage = 0;
+  }
+  updateCarousel();
+}
+
+function prevSlide() {
+  if (currentPage > 0) {
+    currentPage--;
+  } else {
+    currentPage = Math.ceil(document.querySelectorAll('.carousel-item').length / itemsPerPage) - 1;
+  }
+  updateCarousel();
+}
+
+function updateCarousel() {
+  const itemWidth = document.querySelector('.carousel-item').offsetWidth;
+  const newTransformValue = -currentPage * itemsPerPage * itemWidth + 'px';
+  document.getElementById('carousel').style.transform = 'translateX(' + newTransformValue + ')';
+}
