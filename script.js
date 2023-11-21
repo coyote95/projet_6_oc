89854342355f16feb75fa5611170f3fb0b40049f
coyote95 <marie.guillot.oc@gmail.com>
2023-11-21 19:23:30 +0100

    charger 2 pages start

--- script.js
+++ script.js
@@ -1,71 +1,65 @@
-const itemsPerPage = 1; // Défiler une image à la fois
-let currentImage = 0; // Page actuelle
-const maxPages = 10; // Nombre maximum de pages à charger
-let nexturl='http://localhost:8000/api/v1/titles/'
-let conteur_next=0;
+const itemsPerPage = 1;
+let currentImage = 0;
+const maxPages = 2; // Chargez seulement deux pages au démarrage
+let nexturl = 'http://localhost:8000/api/v1/titles/';
+let conteur_next = 0;
+let start = 0;
+let loadedPages = 1; // Nouvelle variable pour suivre le nombre de pages chargées
 
-function displayMovieImages(url) {
-  
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        const movies = data.results;
+function displayMovieImages(url,) {
+  console.log(url);
 
-        // Sélectionner l'élément conteneur pour le carrousel
-        const carouselContainer = document.getElementById('carousel');
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const movies = data.results;
+      const carouselContainer = document.getElementById('carousel');
 
-        // Ajouter les images au carrousel
-        movies.forEach(function (movie) {
-          const carouselItem = document.createElement('div');
-          carouselItem.className = 'carousel-item';
-          const img = document.createElement('img');
-          img.src = movie.image_url;
-          img.alt = movie.title;
-          img.onclick = function () {
-            openModal(movie.title, movie.director, movie.year);
+      movies.forEach(function (movie) {
+        const carouselItem = document.createElement('div');
+        carouselItem.className = 'carousel-item';
+        const img = document.createElement('img');
+        img.src = movie.image_url;
+        img.alt = movie.title;
+        img.onclick = function () {
+          openModal(movie.title, movie.director, movie.year);
         };
-          carouselItem.appendChild(img);
-          carouselContainer.appendChild(carouselItem);
-        });
-        nexturl=data.next
-        // // Vérifier s'il y a une page suivante
-        // if (data.next !== null) {
-        //   // Appel récursif pour la page suivante seulement si la limite n'est pas atteinte
-        //   currentImage++;
-        //   console.log(currentImage);
-        //   console.log(data.next);
-        //   displayMovieImages(data.next);
-        // } else {
-        //   // Mettre à jour le carrousel une fois toutes les images chargées
-        //   updateCarousel();
-        // }
-      })
-      .catch(error => {
-        console.error('Erreur lors de la requête Fetch', error);
+        carouselItem.appendChild(img);
+        carouselContainer.appendChild(carouselItem);
       });
-  
+
+      nexturl = data.next;
+
+      if ((data.next !== null) && (start !== 0) && (loadedPages < maxPages)) {
+        displayMovieImages(nexturl);
+        updateCarousel();
+        loadedPages++;
+      } else {
+        updateCarousel();
+      }
+    })
+    .catch(error => {
+      console.error('Erreur lors de la requête Fetch', error);
+    });
 }
 
 // Appel initial avec l'URL de la première page
-
-displayMovieImages('http://localhost:8000/api/v1/titles/');
-displayMovieImages("http://localhost:8000/api/v1/titles/?page=2");
+displayMovieImages('http://localhost:8000/api/v1/titles/?sort_by=-imdb_score');
+start = 1;
 
 function nextSlide() {
   conteur_next++;
-  if (conteur_next>=5)
- 
-   {displayMovieImages(nexturl);
-    conteur_next=0;
+
+  if (conteur_next >= 5) {
+    displayMovieImages(nexturl);
+    conteur_next = 0;
   }
+
   const carouselItems = document.querySelectorAll('.carousel-item');
   const pageCount = Math.ceil(carouselItems.length / itemsPerPage);
- 
+
   if (currentImage < pageCount - 1) {
     currentImage++;
-    
-    console.log("currente image:"+currentImage)
-    console.log("totale image:"+pageCount)
   } else {
     currentImage = 0;
   }
@@ -88,11 +82,11 @@ function updateCarousel() {
 }
 
 function openModal(title, director, year) {
-    const modal = document.getElementById('myModal');
-    const modalContent = document.getElementById('modalContent');
+  const modal = document.getElementById('myModal');
+  const modalContent = document.getElementById('modalContent');
 
-    modal.style.display = 'block';
-    modalContent.innerHTML = `
+  modal.style.display = 'block';
+  modalContent.innerHTML = `
         <h2>${title}</h2>
         <p>Réalisateur: ${director}</p>
         <p>Année de sortie: ${year}</p>
@@ -100,8 +94,7 @@ function openModal(title, director, year) {
     `;
 }
 
-// Fonction pour fermer la fenêtre modale
 function closeModal() {
-    const modal = document.getElementById('myModal');
-    modal.style.display = 'none';
+  const modal = document.getElementById('myModal');
+  modal.style.display = 'none';
 }
