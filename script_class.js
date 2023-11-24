44d25136e299ec2a44e70c11c12c2594ce1f7578
coyote95 <marie.guillot.oc@gmail.com>
2023-11-24 15:35:42 +0100

    corrction modal

--- script_class.js
+++ script_class.js
@@ -1,134 +1,123 @@
 class Carousel {
     constructor(sectionId, initialUrl) {
-      this.sectionId = sectionId;
-      this.itemsPerPage = 1;
-      this.currentImage = 0;
-      this.nextUrl = initialUrl;
-      this.start = 0;
-      this.compteur=0;
-      
-  
-      this.init();
+        this.sectionId = sectionId;
+        this.itemsPerPage = 1;
+        this.currentImage = 0;
+        this.nextUrl = initialUrl;
+        this.start = 0;
+        this.compteur = 0;
+        this.init();
     }
-  
+
     init() {
-      this.displayMovieImages();
-  
-      const nextButton = document.getElementById(`nextButton${this.sectionId}`);
-      const prevButton = document.getElementById(`prevButton${this.sectionId}`);
-  
-      nextButton.addEventListener('click', () => this.nextSlide());
-      prevButton.addEventListener('click', () => this.prevSlide());
+        this.displayMovieImages();
+
+        const nextButton = document.getElementById(`nextButton${this.sectionId}`);
+        const prevButton = document.getElementById(`prevButton${this.sectionId}`);
+
+        nextButton.addEventListener('click', () => this.nextSlide());
+        prevButton.addEventListener('click', () => this.prevSlide());
     }
-  
+
     displayMovieImages() {
-      
         const fetchAndDisplay = async (url) => {
-          try {
-            const response = await fetch(url);
-      
-            if (!response.ok) {
-              throw new Error(`Erreur HTTP ! Status: ${response.status}`);
+            try {
+                const response = await fetch(url);
+
+                if (!response.ok) {
+                    throw new Error(`Erreur HTTP ! Status: ${response.status}`);
+                }
+
+                const data = await response.json();
+
+                if (!data.results) {
+                    throw new Error('Réponse de l\'API incorrecte. Aucun résultat trouvé.');
+                }
+
+                this.nextUrl = data.next;
+
+                const movies = data.results;
+                const carouselContainer = document.getElementById(`carousel-container${this.sectionId}`);
+                const carousel = carouselContainer.querySelector('.carousel');
+
+                movies.forEach(movie => {
+                    const carouselItem = document.createElement('div');
+                    carouselItem.className = 'carousel-item';
+                    const img = document.createElement('img');
+                    img.src = movie.image_url;
+                    img.alt = movie.title;
+                    img.onclick = (event) => this.openModal(movie.url, event);
+                    carouselItem.appendChild(img);
+                    carousel.appendChild(carouselItem);
+                });
+
+                this.updateCarousel();
+            } catch (error) {
+                console.error('Erreur lors de la requête Fetch', error);
             }
-      
-            const data = await response.json();
-      
-            if (!data.results) {
-              throw new Error('Réponse de l\'API incorrecte. Aucun résultat trouvé.');
-            }
-      
-            this.nextUrl = data.next;
-      
-            const movies = data.results;
-            const carouselContainer = document.getElementById(`carousel-container${this.sectionId}`);
-            const carousel = carouselContainer.querySelector('.carousel');
-      
-            movies.forEach(movie => {
-              const carouselItem = document.createElement('div');
-              carouselItem.className = 'carousel-item';
-              const img = document.createElement('img');
-              img.src = movie.image_url;
-              img.alt = movie.title;
-              img.onclick = () => this.openModal(movie.url);
-              carouselItem.appendChild(img);
-              carousel.appendChild(carouselItem);
-            });
-      
-            this.updateCarousel();
-          } catch (error) {
-            console.error('Erreur lors de la requête Fetch', error);
-          }
         };
-      
+
         // Utilisation de await pour attendre la fin de chaque fetch
         (async () => {
-          await fetchAndDisplay(this.nextUrl); // Fetch de la première page
-          await fetchAndDisplay(this.nextUrl + "&page=2"); // Fetch de la deuxième page
+            await fetchAndDisplay(this.nextUrl); // Fetch de la première page
+            await fetchAndDisplay(this.nextUrl + "&page=2"); // Fetch de la deuxième page
         })();
-      }
-      
-      
-      
-  
+    }
+
     fetchMovies() {
         fetch(this.nextUrl)
-          .then(response => response.json())
-          .then(data => {
-            this.nextUrl = data.next;
-      
-            const movies = data.results;
-            const carouselContainer = document.getElementById(`carousel-container${this.sectionId}`);
-            const carousel = carouselContainer.querySelector('.carousel');
-      
-          
-      
-            movies.forEach(movie => {
-              const carouselItem = document.createElement('div');
-              carouselItem.className = 'carousel-item';
-              const img = document.createElement('img');
-              img.src = movie.image_url;
-              img.alt = movie.title;
-              img.onclick = () => this.openModal(movie.url);
-              carouselItem.appendChild(img);
-              carousel.appendChild(carouselItem);
-            });
-      
-           
-          })
-          .catch(error => console.error('Erreur lors de la requête Fetch', error));
-      }
-      
-  
-      nextSlide() {
+            .then(response => response.json())
+            .then(data => {
+                this.nextUrl = data.next;
+
+                const movies = data.results;
+                const carouselContainer = document.getElementById(`carousel-container${this.sectionId}`);
+                const carousel = carouselContainer.querySelector('.carousel');
+
+                movies.forEach(movie => {
+                    const carouselItem = document.createElement('div');
+                    carouselItem.className = 'carousel-item';
+                    const img = document.createElement('img');
+                    img.src = movie.image_url;
+                    img.alt = movie.title;
+                    img.onclick = (event) => this.openModal(movie.url, event);
+                    carouselItem.appendChild(img);
+                    carousel.appendChild(carouselItem);
+                });
+
+            })
+            .catch(error => console.error('Erreur lors de la requête Fetch', error));
+    }
+
+    nextSlide() {
         this.compteur++;
-        console.log('0k');
         this.currentImage++;
-        if (this.compteur>=4){
-            this.fetchMovies(); 
-            this.compteur=0;
+        if (this.compteur >= 4) {
+            this.fetchMovies();
+            this.compteur = 0;
         }
-        
+
         const carouselItems = document.querySelectorAll(`#carousel-container${this.sectionId} .carousel-item`);
         const pageCount = Math.ceil(carouselItems.length / this.itemsPerPage);
-      
+
         if (this.currentImage >= pageCount) {
-          this.currentImage = 0;
+            this.currentImage = 0;
         }
-      
+
         this.updateCarousel();
-      }
-  
-    prevSlide() {
-      this.currentImage--;
-      const pageCount = Math.ceil(document.querySelectorAll(`#carousel-container${this.sectionId} .carousel-item`).length / this.itemsPerPage);
-  
-      if (this.currentImage < 0) {
-        this.currentImage = pageCount - 1;
-      }
-  
-      this.updateCarousel();
     }
-  
+
+    prevSlide() {
+        this.currentImage--;
+        const pageCount = Math.ceil(document.querySelectorAll(`#carousel-container${this.sectionId} .carousel-item`).length / this.itemsPerPage);
+
+        if (this.currentImage < 0) {
+            this.currentImage = pageCount - 1;
+        }
+
+        this.updateCarousel();
+    }
+
     updateCarousel() {
         const itemWidth = document.querySelector(`#carousel-container${this.sectionId} .carousel-item`).offsetWidth;
         const newTransformValue = -this.currentImage * this.itemsPerPage * itemWidth + 'px';
@@ -137,9 +126,9 @@ class Carousel {
 
         if (carouselContainer) {
             carouselContainer.querySelector('.carousel').style.transform = 'translateX(' + newTransformValue + ')';
-            
+
             // Toggle visibility of prevButton based on currentImage value
-            if (this.currentImage === 0) {
+            if (this.currentImage == 0) {
                 prevButton.style.visibility = 'hidden';
             } else {
                 prevButton.style.visibility = 'visible';
@@ -148,44 +137,63 @@ class Carousel {
             console.error(`Carousel container with ID 'carousel-container${this.sectionId}' not found.`);
         }
     }
-  
-    openModal(urlfilm) {
-        fetch(urlfilm)
-        .then(response => response.json())
-        .then(data => {
-           
-    
-            const modal = document.getElementById(`myModal${this.sectionId}`);
-            const modalContent = document.getElementById(`modalContent${this.sectionId}`);
-    
-            modal.style.visibility = 'visible';
-            modalContent.innerHTML = `
-                <button onclick="closeModal('${this.sectionId}')">X</button>
-                <h2>${data.title}</h2>
-                <img src=${data.image_url} alt=${data.title}</img>
-                <p>Genres: ${data.genres}</p>
-                <p>Année de sortie: ${data.year}</p>
-                <p>Rated:${data.rated}</p>
-                <p>Score Imdb:${data.imdb_score}</p>
-                <p>Réalisateur:${data.directors}</p>
-                <p>Acteurs:${data.actors}</p>
-                <p>Durée:${data.duration}</p>
-                <p>Origine:${data.countries}</p>
-                <p>Box Office:${data.worldwide_gross_income}</p>
-                <p>Résumé:${data.description}</p>
-            `;
-        })
-        .catch(error => {
-            console.error('Erreur lors de la requête Fetch', error);
-        });
-    }
-    
-  }
 
-  document.addEventListener('DOMContentLoaded', function () {
+    openModal(urlfilm, event) {
+       
+        if ( document.querySelector('.modal')==null)
+        {  
+       
+        fetch(urlfilm)
+            .then(response => response.json())
+            .then(data => {
+                if (event) {
+                    var mouseX = event.clientX;
+                    var mouseY = event.clientY;
+
+                    const modal = document.createElement('div');
+                    modal.className = 'modal';
+                    document.body.appendChild(modal);
+
+                    const modalContent = document.createElement('div');
+                    modalContent.className = 'modal-content';
+                    modal.appendChild(modalContent);
+
+                    modalContent.innerHTML = `
+                        <button onclick="closeModal()">X</button>
+                        <h2>${data.title}</h2>
+                        <img src=${data.image_url} alt=${data.title}></img>
+                        <p>Genres: ${data.genres}</p>
+                        <p>Année de sortie: ${data.year}</p>
+                        <p>Rated:${data.rated}</p>
+                        <p>Score Imdb:${data.imdb_score}</p>
+                        <p>Réalisateur:${data.directors}</p>
+                        <p>Acteurs:${data.actors}</p>
+                        <p>Durée:${data.duration}</p>
+                        <p>Origine:${data.countries}</p>
+                        <p>Box Office:${data.worldwide_gross_income}</p>
+                        <p>Résumé:${data.description}</p>
+                    `;
+
+                    // Définir la position de la modal en fonction des coordonnées de la souris
+                    modal.style.left = mouseX + 'px';
+                    modal.style.top = mouseY + 'px';
+
+                    // Afficher la modal
+                    modal.style.visibility = 'visible';
+                } else {
+                    console.error('Event object is undefined or null.');
+                }
+            })
+            .catch(error => {
+                console.error('Erreur lors de la requête Fetch', error);
+            });
+            
+    }
+}
+}
+
+document.addEventListener('DOMContentLoaded', function () {
     const afficheSection = document.getElementById('afficheSection');
-    const modalAffiche = document.getElementById('myModalaffiche');
-    const modalContentAffiche = document.getElementById('modalContentAffiche');
 
     // URL de l'API pour obtenir le meilleur film
     const apiUrl = 'http://localhost:8000/api/v1/titles/?sort_by=-imdb_score';
@@ -203,7 +211,7 @@ class Carousel {
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-             bestMovie = data.results[0];
+            bestMovie = data.results[0];
             updateAffiche(bestMovie);
         })
         .catch(error => {
@@ -211,39 +219,46 @@ class Carousel {
         });
 
     // Ajoutez un gestionnaire d'événements au conteneur de la section "affiche"
-    afficheSection.addEventListener('click', function () {
-        openModalInBackground('myModalaffiche');
+    afficheSection.addEventListener('click', function (event) {
+        openModalInBackground(event);
     });
 
     // Fonction pour ouvrir la fenêtre modale en arrière-plan
-    function openModalInBackground(modalId) {
-        const modal = document.getElementById(modalId);
-        modal.style.visibility = 'visible';
+    function openModalInBackground(event) {
+       
+        if(document.querySelector('.modal')==null){
+            if (event) {
+                var mouseX = event.clientX;
+                var mouseY = event.clientY;
+        const modal = document.createElement('div');
+        modal.className = 'modal';
+        document.body.appendChild(modal);
 
-        // Récupérez les caractéristiques du film
-        fetch(bestMovie.url)
-            .then(response => response.json())
-            .then(data => {
-                // Remplissez le contenu de la fenêtre modale avec les caractéristiques du film
-                modalContentAffiche.innerHTML = `
-                    <button onclick="closeModalAffiche('${modalId}')">X</button>
-                    <h2>${data.title}</h2>
-                    <img src=${data.image_url} alt=${data.title}></img>
-                    <p>Genres: ${data.genres}</p>
-                    <p>Année de sortie: ${data.year}</p>
-                    <p>Rated:${data.rated}</p>
-                    <p>Score Imdb:${data.imdb_score}</p>
-                    <p>Réalisateur:${data.directors}</p>
-                    <p>Acteurs:${data.actors}</p>
-                    <p>Durée:${data.duration}</p>
-                    <p>Origine:${data.countries}</p>
-                    <p>Box Office:${data.worldwide_gross_income}</p>
-                    <p>Résumé:${data.description}</p>
-                `;
-            })
-            .catch(error => {
-                console.error('Erreur lors de la requête Fetch', error);
-            });
+        const modalContentAffiche = document.createElement('div');
+        modalContentAffiche.className = 'modal-content';
+        modal.appendChild(modalContentAffiche);
+
+        modalContentAffiche.innerHTML = `
+            <button onclick="closeModal()">X</button>
+            <h2>${bestMovie.title}</h2>
+            <img src=${bestMovie.image_url} alt=${bestMovie.title}></img>
+            <p>Genres: ${bestMovie.genres}</p>
+            <p>Année de sortie: ${bestMovie.year}</p>
+            <p>Rated:${bestMovie.rated}</p>
+            <p>Score Imdb:${bestMovie.imdb_score}</p>
+            <p>Réalisateur:${bestMovie.directors}</p>
+            <p>Acteurs:${bestMovie.actors}</p>
+            <p>Durée:${bestMovie.duration}</p>
+            <p>Origine:${bestMovie.countries}</p>
+            <p>Box Office:${bestMovie.worldwide_gross_income}</p>
+            <p>Résumé:${bestMovie.description}</p>
+        `;
+        modal.style.left = mouseX + 'px';
+        modal.style.top = mouseY + 'px';
+
+        modal.style.visibility = 'visible';
+            }
+        }
     }
 
     // Initialize carousels
@@ -253,11 +268,7 @@ class Carousel {
     const carousel4 = new Carousel('4', 'http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&genre=Horror');
 });
 
-function closeModal(sectionId) {
-    const modal = document.getElementById(`myModal${sectionId}`);
-    modal.style.display = 'none';
-}
-function closeModalAffiche(modalAffiche) {
-    const modal = document.getElementById(`${modalAffiche}`);
-    modal.style.display = 'none';
+function closeModal() {
+    const modal = document.querySelector('.modal');
+    document.body.removeChild(modal);
 }
